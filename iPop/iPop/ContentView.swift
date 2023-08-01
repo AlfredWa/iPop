79e0b9202b100ef3770ae81cffe49d3c38e7162a
AlfredWa <bowenwang3@cmail.carleton.ca>
2023-08-01 01:09:15 -0400

    show user location

--- iPop/iPop/ContentView.swift
+++ iPop/iPop/ContentView.swift
@@ -8,26 +8,49 @@
 import SwiftUI
 import MapKit
 
+struct Landmark: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
 struct ContentView: View {
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
     
+    let landmarks = {
+        Landmark(name: "Alfred", coordinate: CLLocationCoordinate2D(latitude: 38, longitude: -77))
+    }
+    
+    
+    
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(
+            coordinateRegion: $region,
+            showsUserLocation: true
+        )
             .edgesIgnoringSafeArea(.all)
             .onAppear {
                 // 在视图加载时获取用户的位置
                 getLocation()
+                
             }
     }
     func getLocation() {
             // 创建一个 Core Location 管理器
             let locationManager = CLLocationManager()
+        if locationManager.authorizationStatus == .authorizedWhenInUse || locationManager.authorizationStatus ==
+            .authorizedAlways {
+              // 已经获取到权限
+            }
+        else{
             locationManager.requestWhenInUseAuthorization()
+        }
 
             // 获取当前设备的位置
             if let location = locationManager.location {
                 region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
             }
+        locationManager.startUpdatingLocation()
         }
 }
 
