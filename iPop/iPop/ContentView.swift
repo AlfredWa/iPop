7785c48a9bda5d579d7a461665f1c39cb06d186f
AlfredWa <bowenwang3@cmail.carleton.ca>
2023-08-02 03:04:15 -0400

    添加async await等用户给权限

--- iPop/iPop/ContentView.swift
+++ iPop/iPop/ContentView.swift
@@ -31,27 +31,37 @@ struct ContentView: View {
             .edgesIgnoringSafeArea(.all)
             .onAppear {
                 // 在视图加载时获取用户的位置
-                getLocation()
+                print("latitude: ", region.center.latitude)
+                Task {
+                    await getLocation()
+                }
+                print("latitude after get location: ", region.center.latitude)
                 
             }
     }
-    func getLocation() {
-            // 创建一个 Core Location 管理器
-            let locationManager = CLLocationManager()
-        if locationManager.authorizationStatus == .authorizedWhenInUse || locationManager.authorizationStatus ==
-            .authorizedAlways {
-              // 已经获取到权限
-            }
-        else{
-            locationManager.requestWhenInUseAuthorization()
+    func getLocation() async throws -> String{
+        // 创建一个 Core Location 管理器
+        let locationManager = CLLocationManager()
+        
+        switch locationManager.authorizationStatus{
+        case .authorized:
+            print("定位授权已允许")
+            locationManager.startUpdatingLocation()
+        case .denied:
+            print("定位授权已拒绝")
+        case .notDetermined:
+            print("定位授权尚未决定")
+            try await locationManager.requestWhenInUseAuthorization()
+        default:
+            break
         }
-
-            // 获取当前设备的位置
-            if let location = locationManager.location {
-                region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
-            }
-        locationManager.startUpdatingLocation()
+        // 获取当前设备的位置
+        if let location = locationManager.location {
+            region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+            print("in if!!!!!!!!!")
         }
+        //locationManager.startUpdatingLocation()
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
